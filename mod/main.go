e5fbc53a1da178e5efda2d0a627ee38f1a853da6
David Helkowski <github@codechild.com>
2021-02-15 17:32:01 -0800

    Use constants
    Return Cmd from AddCmd also.
    Simplify OPT creation using flags instead of using a different function for required.
    Update license
    t

--- mod/main.go
+++ mod/main.go
@@ -6,13 +6,38 @@ import (
   "strconv"
 )
 
+// flags for OPT option creation
+const (
+    REQ = 1
+    FLAG = 2
+)
+
+const (
+    TYPE_FLAG = iota + 1
+)
+
 type Opt struct {
-    optType int
-    val string
-    name string
-    descr string
+    optType  int
+    val      string
+    name     string
+    descr    string
     required bool
 }
+
+type Cmd struct {
+    opts      map[string]*Opt
+    handler   func( *Cmd )
+    argc      int
+    argv      []string
+    descr     string
+    Extrahelp string
+}
+
+type Uclop struct {
+    cmds map[string]*Cmd
+}
+
+type OPTS []*Opt
 
 func (self *Opt) String() string {
     return self.val
@@ -30,40 +55,19 @@ func (self *Opt) Bool() bool {
     return false
 }
 
-func OPT( name string, descr string ) (*Opt) {
-    return &Opt{
-        name: name,
-        descr: descr,
+func OPT( name string, descr string, flags int ) (*Opt) {
+    opt := &Opt{
+        name:     name,
+        descr:    descr,
+        required: ( flags & REQ )==REQ,
     }
-}
-
-func OPT_REQ( name string, descr string ) (*Opt) {
-    opt := OPT( name, descr )
-    opt.required = true
+    if (flags & FLAG)==FLAG {
+        opt.optType = TYPE_FLAG
+    }
     return opt
 }
 
-func OPT_FLAG( name string, descr string ) (*Opt) {
-    opt := OPT( name, descr )
-    opt.optType = 2
-    return opt
-}
-
-type Cmd struct {
-    opts map[string]*Opt
-    handler func( *Cmd )
-    argc int
-    argv []string
-    descr string
-}
-
-type Uclop struct {
-    cmds map[string]*Cmd
-}
-
-type OPTS []*Opt
-
-func (self *Uclop) AddCmd( cmdStr string, descr string, handler func( *Cmd ), opts OPTS ) {
+func (self *Uclop) AddCmd( cmdStr string, descr string, handler func( *Cmd ), opts OPTS ) *Cmd {
     cmd := NewUcmd( descr, handler )
     self.cmds[ cmdStr ] = cmd
     
@@ -73,6 +77,7 @@ func (self *Uclop) AddCmd( cmdStr string, descr string, handler func( *Cmd ), op
             cmd.opts[ oName ] = opt
         }
     }
+    return cmd
 }
 
 func NewUcmd( descr string, handler func(*Cmd) ) ( *Cmd ) {
@@ -111,8 +116,6 @@ func (self *Uclop) Run() {
     if cmd == nil {
         self.Usage()
         return
-    } else {
-        //fmt.Printf("Found cmd %s\n", cmdStr )
     }
     
     // Store arguments
@@ -125,24 +128,24 @@ func (self *Uclop) Run() {
         if arg[0] == '-' {
             opt := cmd.opts[ arg ]
             if opt == nil {
-                fmt.Fprintf( os.Stderr, "Unknown option %s\n", arg )
+                fmt.Printf( "Unknown option %s\n", arg )
                 os.Exit(1)
             }
-            if opt.optType == 2 {
+            if opt.optType == TYPE_FLAG {
                 opt.val = "true"
             } else {
                 i++
                 if i >= argc {
-                  break
+                    break
                 }
                 opt.val = argv[i]
             }
             i++
             if i >= argc {
-              break
+                break
             }
         } else {
-          break
+            break
         }
     }
     cmd.argc = argc - i + 1
@@ -169,18 +172,18 @@ func runTest( cmd *Cmd ) {
 func (self *Cmd) UsageInline() {
     for optName, opt := range self.opts {
         if opt.required {
-            fmt.Printf(" %s val", optName )
+            fmt.Printf(" %s val", optName)
         } else {
-            if opt.optType == 2 {
-                fmt.Printf(" [%s]", optName )
+            if opt.optType == TYPE_FLAG {
+                fmt.Printf(" [%s]", optName)
             } else {
-                fmt.Printf(" [%s val]", optName )
+                fmt.Printf(" [%s val]", optName)
             }
         }
     }
-    //if self.extrahelp {
-    //    fmt.Printf(" %s", self.extrahelp )
-    //}
+    if self.Extrahelp != "" {
+        fmt.Printf(" %s", self.Extrahelp )
+    }
     fmt.Printf("\n")
 }
 
@@ -195,18 +198,23 @@ func (self *Opt) Usage() {
     if self.required {
         reqStr = "REQUIRED"
     }
-    fmt.Printf("  %s %s\n", self.name, reqStr )
-    fmt.Printf("    %s\n", self.descr );
+    fmt.Printf("  %s %s\n", self.name, reqStr)
+    fmt.Printf("    %s\n", self.descr)
 }
 
 func (self *Uclop) Usage() {
-    fmt.Printf("Usage:\n");
+    fmt.Printf("Usage:\n")
     
-    prog := "test"//os.Args[0]
+    // Get program name, stripping off path
+    prog := os.Args[0]
+    j := 0
+    for i:=0 ; i<len(prog) ; i++ { if prog[i]=='/' { j = i } }
+    if j>0 { prog = prog[j+1:] }
+    
     for cmdName, cmd := range self.cmds {
-        fmt.Printf("%s %s", prog, cmdName )
+        fmt.Printf( prog + " " + cmdName )
         cmd.UsageInline()
-        fmt.Printf("  %s\n", cmd.descr )
+        fmt.Printf("  %s\n", cmd.descr)
         cmd.Usage()
     }
 }
@@ -216,7 +224,7 @@ func (self *Cmd ) ensureRequired() {
     for optName, opt := range self.opts {
         if opt.required && opt.val == "" {
             missing = true
-            fmt.Fprintf(os.Stderr,"Required option %s missing\n", optName )
+            fmt.Printf("Required option %s missing\n", optName)
             opt.Usage()
         }
     }
